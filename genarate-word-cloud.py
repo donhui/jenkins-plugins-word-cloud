c20ac8879aa11a80f49911457e499b0b244ee41b
donhui <977675308@qq.com>
2020-06-18 18:06:08 +0800

    add more background images

--- genarate-word-cloud.py
+++ genarate-word-cloud.py
@@ -6,20 +6,25 @@ import numpy as np
 from PIL import Image
 
 
+def generate_word_cloud_image(background_image):
+    # mask
+    mask = np.array(Image.open(background_image))
+
+    # generate word cloud
+    wc = WordCloud(mask=mask, scale=1.5, mode='RGBA', background_color="white", max_words=2000).generate(text=text)
+
+    # show word cloud
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+    # save to file
+    wc.to_file(image_name.replace(".png", "-") + 'word-cloud.png')
+
+
 if __name__ == "__main__":
     with open('jenkins-plugins.txt') as f:
         text = f.read()
 
-        # mask
-        mask = np.array(Image.open("jenkins-horizontal-color.png"))
-
-        # generate word cloud
-        wc = WordCloud(mask=mask, scale=1.5, mode='RGBA', background_color="white", max_words=2000).generate(text=text)
-
-        # show word cloud
-        plt.imshow(wc, interpolation='bilinear')
-        plt.axis('off')
-        plt.show()
-
-        # save to file
-        wc.to_file('word-cloud.png')
+        for image_name in ["kongfu.png", "jenkins-logo.png"]:
+            generate_word_cloud_image(background_image=image_name)
